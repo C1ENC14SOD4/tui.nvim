a29ec2a332f7c0620b6b4ea681900e9b7a094448
C1ENC14SOD4 <johnthesk23@gmail.com>
2023-11-24 15:25:01 +0000

    Simple but powerful changes.

---
+++ lua/tui/ui/window.lua
@@ -0,0 +1,66 @@
+---@class Widnow
+---@field private bufnr? integer
+---@field private window? integer
+local M = {}
+
+---@param options Options
+---@param extra_options ExtraOptions
+---@return self
+function M:new(options, extra_options)
+  local obj = {}
+  setmetatable(obj, self)
+  self.__index = self
+  self.bufnr = vim.api.nvim_create_buf(false, true)
+  self.window = vim.api.nvim_open_win(self.bufnr, extra_options.enter_to_window, {
+    relative = 'editor',
+    width = options.size.width,
+    height = options.size.height,
+    row = options.position.row,
+    col = options.position.col,
+    style = 'minimal',
+    title = {
+      { options.title.text, options.title.highlight }
+    },
+    title_pos = options.title.position,
+    border = options.border,
+  })
+  return obj
+end
+
+---@class Options
+---@field public size Size
+---@field public position Position
+---@field public title Title
+---@field public border? Border
+
+---@class ExtraOptions
+---@field public insert_on_init boolean
+---@field public enter_to_window boolean
+
+---@class Size
+---@field public width integer
+---@field public height integer
+
+---@class Title
+---@field public text? string
+---@field public highlight? string
+---@field public position? TitlePosition
+
+---@class Position
+---@field public row integer
+---@field public col integer
+
+---@alias TitlePosition
+---| '"center"'
+---| '"left"'
+---| '"right"'
+
+---@alias Border
+---| '"none"'
+---| '"single"'
+---| '"rounded"'
+---| '"double"'
+---| '"shadow"'
+---| '"solid"'
+
+return M
